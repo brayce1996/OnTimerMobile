6ad8a8cacc6a8f4375573753493f4cd045d4097a
Wei <wwang107@gmail.com>
2018-05-12 00:20:34 +0800

    apply icon at navbar

--- App.js
+++ App.js
@@ -1,35 +1,53 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { createBottomTabNavigator } from 'react-navigation';
 import Home from './src/components/HomeScreen'
 import EventForm from './src/components/EventForm'
 import Account from './src/components/AccountScreen'
 import Notification from './src/components/NotificationScreen'
 
-// class HomeScreen extends React.Component {
-//   render() {
-//     return (
-//       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//         <Text>Home!</Text>
-//       </View>
-//     );
-//   }
-// }
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 
-// class SettingsScreen extends React.Component {
-//   render() {
-//     return (
-//       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//         <Text>Settings!</Text>
-//       </View>
-//     );
-//   }
-// }
+export default class App extends React.Component {
+  render() {
+    return <AppNavigator />
+  }
+}
 
-export default createBottomTabNavigator({
+const AppNavigator = createBottomTabNavigator({
   Home: Home,
-  NewEvent: EventForm,
+  'Add Event': EventForm,
   Notification: Notification,
   Account: Account
+},
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'Home'){
+          iconName = `ios-menu${focused ? '' : '-outline'}`;
+        }else if (routeName === 'Add Event'){
+          iconName = `ios-add${focused ? '' : '-outline'}`;
+        } else if (routeName === 'Notification') {
+          iconName = `ios-notifications${focused ? '' : '-outline'}`;
+        } else if (routeName === 'Account'){
+          iconName = `ios-contact${focused ? '' : '-outline'}`;
+        }
+        
+        
+        
 
-});
+        // You can return any component that you like here! We usually use an
+        // icon component from react-native-vector-icons
+        return <Ionicons name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'black',
+      inactiveTintColor: 'gray',
+    },
+  }
+);
